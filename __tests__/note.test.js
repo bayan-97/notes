5615d092710da853ed766a5ea89873a713bfa6d2
bayanalalem <bayanalalem0@gmail.com>
2020-10-12 21:26:46 +0300

    done

--- __tests__/note.test.js
+++ __tests__/note.test.js
@@ -16,7 +16,7 @@ it('returns note isnot showed ', () => {
   const options = new Input();
 
   let optiones1 = new Input2(options);
-  optiones1.notrText = true;
+  optiones1.notrText = '';
 
   optiones1.add();
   expect(console.log).not.toHaveBeenCalledWith();
