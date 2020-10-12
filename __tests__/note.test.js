85986ed20bfa1d2fc1cb4d52bb54f3440b4c2e58
bayanalalem <bayanalalem0@gmail.com>
2020-10-12 19:38:27 +0300

    done

---
+++ __tests__/note.test.js
@@ -0,0 +1,26 @@
+'use strict';
+const Input = require('../lib/input.js');
+const Input2 = require('../lib/note.js');
+jest.spyOn(global.console, 'log');
+
+describe('apperance note to the user ', () => {
+  it('returns the note is showed', () => {
+    const options = new Input();
+    let optiones1 = new Input2(options);
+   
+
+    optiones1.add();
+    expect(console.log).toHaveBeenCalled();
+  });
+});
+
+it('returns note isnot showed ', () => {
+  const options = new Input();
+
+  let optiones1 = new Input2(options);
+  optiones1.notrText =true;
+
+  optiones1.add();
+  expect(console.log).not.toHaveBeenCalledWith();
+});
+
